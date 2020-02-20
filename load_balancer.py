d5603be10250378dac29f49cee6d371f56a0000a
flavkaze <flav.gaspareto@gmail.com>
2020-02-20 05:36:14 -0300

    adjust code in method  process and others

--- load_balancer.py
+++ load_balancer.py
@@ -1,16 +1,17 @@
-"""
-para teste unitarios
+# """
+# para teste unitarios
 
-ttask tem q ser entre 1 e 10
-umax tem q ser entre 1 e 10
+# ttask tem q ser entre 1 e 10
+# umax tem q ser entre 1 e 10
 
-input > remove > conta o tick > output
+# input > remove > conta o tick > output
 
-fila de saida =  en qual loop aquela task vai sair (especificar em qual server eles estava)
+# fila de saida =  en qual loop aquela task vai sair (especificar em qual server eles estava)
 
-lista de servers
+# lista de servers
 
-"""
+# """
+import os
 
 class Machine:
     
@@ -21,30 +22,30 @@ class Machine:
         self.open_slots = umax
 
     def close_processed_task(self, counter_tick):
-        for position, each_task in enumerate(self.tasks_death_tick, 0):
+        new_list = self.tasks_death_tick.copy()
+        for each_task in self.tasks_death_tick:
             if each_task == counter_tick:
-                self.tasks_death_tick.pop(position)
+                new_list.remove(each_task)
                 self.open_slots += 1
+                self.open_tasks -= 1
     
     def up_tasks(self, amount_task, number_tasks, ttask):
         for each_task in range(number_tasks):
             if self.open_tasks < self.max_capacity:
-                self.tasks_death_tick.append(amount_task + (ttask - 1))
+                self.tasks_death_tick.append(amount_task + (ttask))
                 self.open_tasks += 1
+                self.open_slots -= 1
                 number_tasks -= 1
             else:
                 return number_tasks
 
         return 0
 
-
-# 
-    
     
 class LoadBalancer:
 
     def __init__(self):
-        self.open_machenes = []
+        self.open_machines = []
         self.ticks = None
         self.ttask = None
         self.umax = None
@@ -60,16 +61,16 @@ class LoadBalancer:
         return list_args
 
     def set_output(self, current_costs):
-        str_costs = [str(x) for x in current_costs]
-        line = ",".join(str_costs)
-        with open(self.output_file_name, "w")as file:
-            file.write(line)
+        str_costs = [str(x) for x in current_costs if x != 0]
+        line = ",".join(str_costs) + "\n"
+        with open(self.output_file_name, "a")as file:
+            file.writelines(line)  
 
     def begin_process(self):
         list_args = self.get_inputs()
 
         #todos os ticks que irao executar
-        self.ticks = [int(x) for x in list_args]
+        self.ticks = [int(x) for x in list_args if x.isdigit()]
         #tamanho em ticks de uma tarefa
         self.ttask = self.ticks.pop(0)
         #numero maximo de usuarios por servidores
@@ -77,29 +78,42 @@ class LoadBalancer:
     
     def process(self):
         for counter_tick, amount_task in enumerate(self.ticks, 1):
+            if counter_tick > 5:
+                print(counter_tick)
             machines_open_tasks = []
-            if not self.open_machenes and amount_task > 0:
-                self.open_machenes.append(Machine(self.umax))
+            if not self.open_machines and amount_task > 0:
+                self.open_machines.append(Machine(self.umax))
             
-            for each_machine in self.open_machenes:
+            for each_machine in self.open_machines:
+
+                each_machine.close_processed_task(counter_tick)
+
                 while amount_task:
                     amount_task = each_machine.up_tasks(
                         counter_tick, 
                         amount_task, 
                         self.ttask)
-                    if amount_task > 0:
-                        self.open_machenes.append(Machine(self.umax))
+                    if all([
+                        amount_task > 0,
+                        each_machine.open_tasks == self.umax,
+                        self.open_machines.index(
+                            each_machine) == len(self.open_machines) - 1]):
+
+                        self.open_machines.append(Machine(self.umax))
+                        break
                     else:
                         break
-                
-                each_machine.close_processed_task(counter_tick)
+                # if amount_task > 0 and each_machine.open_tasks > self.umax:
+
                 machines_open_tasks.append(
                     each_machine.open_tasks)
 
-            self.total_cost += sum(machines_open_tasks)
+            self.total_cost += len(machines_open_tasks)
             self.set_output(machines_open_tasks)
 
     def run(self):
+        if os.path.exists("output.txt"):
+            os.remove("output.txt")
         self.begin_process()
         self.process()
 
