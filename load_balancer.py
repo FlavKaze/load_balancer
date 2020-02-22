ec8a12b9ed5ae06b105510d49a1f48285c0bdf13
flavkaze <flav.gaspareto@gmail.com>
2020-02-21 21:22:46 -0300

    Add coments, add tests, add ci

--- load_balancer.py
+++ load_balancer.py
@@ -1,27 +1,46 @@
-# """
-# para teste unitarios
+"""This script has to simulate a load-balancing task run on a cluster ."""
 
-# ttask tem q ser entre 1 e 10
-# umax tem q ser entre 1 e 10
+__author__ = ['Flavio Augusto Gaspareto']
+__version__ = '1.0'
+__maintainer__ = ['Flavio Augusto Gaspareto']
+__email__ = ['flav.gaspareto@gmail.com']
+__gitlab__ = ['https://gitlab.com/FlavKaze']
 
-# input > remove > conta o tick > output
-
-# fila de saida =  en qual loop aquela task vai sair (especificar em qual server eles estava)
-
-# lista de servers
-
-# """
 import os
 
+
 class Machine:
-    
-    def __init__(self, umax):
+    """Class that describes the attributes and methods of a machine / server.
+
+    Attributes
+    ----------
+    tasks_death_tick : list
+        List that keeps on which tick that process will end and will have to
+        be removed from the machine.
+    max_capacity : int
+        Maximum limit of simultaneous tasks that the machine supports.
+    open_tasks : int
+        Number of open tasks.
+    open_slots : int
+        Number of slots for tesks available.
+    """
+
+    def __init__(self, umax: int):
+        """Inicia a classe.
+
+        :param umax:
+        """
         self.tasks_death_tick = []
         self.max_capacity = umax
         self.open_tasks = 0
         self.open_slots = umax
 
-    def close_processed_task(self, counter_tick):
+    def close_processed_task(self, counter_tick: int) -> None:
+        """Responsible for closing tasks marked to be killed on that tick.
+
+        :param counter_tick: Counter of ticks from the beginning of
+        the execution.
+        """
         new_list = self.tasks_death_tick.copy()
         for each_task in self.tasks_death_tick:
             if each_task == counter_tick:
@@ -29,11 +48,21 @@ class Machine:
                 self.open_slots += 1
                 self.open_tasks -= 1
         self.tasks_death_tick = new_list
-    
-    def up_tasks(self, amount_task, number_tasks, ttask):
+
+    def up_tasks(self, counter_tick: int, number_tasks: int,
+                 ttask: int) -> int:
+        """Open the maximum number of processes called inside the machine.
+
+        :param counter_tick: Counter of ticks from the beginning of
+        the execution.
+        :param number_tasks: Receive the number of tasks that you will try
+        to open.
+        :param ttask: Task size.
+        :return: Returns the number of tasks not executed.
+        """
         for each_task in range(number_tasks):
             if self.open_tasks < self.max_capacity:
-                self.tasks_death_tick.append(amount_task + (ttask))
+                self.tasks_death_tick.append(counter_tick + ttask)
                 self.open_tasks += 1
                 self.open_slots -= 1
                 number_tasks -= 1
@@ -42,10 +71,33 @@ class Machine:
 
         return 0
 
-    
+
 class LoadBalancer:
+    """Class has the purpose of load balancing servers that run tasks.
+
+    Attributes
+    ----------
+    open_machines : list
+        List of machines that are open to perform tasks.
+    ticks : list
+        Receives the ticks to be processed in that execution.
+    ttask : int
+        Receives the size of the tasks for that execution.
+    umax : int
+        It receives the maximum value of tasks being executed simultaneously
+        in that execution.
+    input_file_name : str
+        Name of the file that will be read to frame the entries.
+    output_file_name : str
+        Name of the file that will be written to the output.
+    total_cost : int
+        Variable that stores the cost spent on the machines being executed.
+    last_machine_id : int
+        Auxiliary variable to assist in the execution of the machines.
+    """
 
     def __init__(self):
+        """Inicia a classe."""
         self.open_machines = []
         self.ticks = None
         self.ttask = None
@@ -53,79 +105,134 @@ class LoadBalancer:
         self.input_file_name = "input.txt"
         self.output_file_name = "output.txt"
         self.total_cost = 0
+        self.last_machine_id = 0
 
-    def get_inputs(self):
+    def get_inputs(self) -> list:
+        """Open the input file captures the lines and transforms in a list.
+
+        :return: Returns a list of entries.
+        """
         with open(self.input_file_name, "r")as file:
             lines = file.read()
             list_args = lines.split("\n")
 
         return list_args
 
-    def set_output(self, current_costs):
-        str_costs = [str(x) for x in current_costs]
-        line = ",".join(str_costs) + "\n"
-        with open(self.output_file_name, "a")as file:
-            file.writelines(line)  
+    def set_output(self, current_costs: [list, str]) -> None:
+        """Receives the cost on that tick and writes in file.
 
-    def begin_process(self):
+        :param current_costs: Parameter that waits the current cost to
+        write to the file.
+        """
+        if isinstance(current_costs, list):
+            str_costs = [str(x) for x in current_costs]
+            line = ",".join(str_costs) + "\n"
+        else:
+            line = str(current_costs)
+        with open(self.output_file_name, "a")as file:
+            file.writelines(line)
+
+    def begin_process(self) -> None:
+        """Adjust the environment before starting the process."""
         list_args = self.get_inputs()
 
-        #todos os ticks que irao executar
         self.ticks = [int(x) for x in list_args if x.isdigit()]
-        #tamanho em ticks de uma tarefa
+
+        if len(self.ticks) < 3:
+            print("The input must have at least 3 lines with integers!")
+            exit()
+
         self.ttask = self.ticks.pop(0)
-        #numero maximo de usuarios por servidores
+        if self.ttask < 1 or self.ttask > 10:
+            print("ttask must be greater than 0 and less than 11!")
+            exit()
+
         self.umax = self.ticks.pop(0)
+        if self.umax < 1 or self.umax > 10:
+            print("umax must be greater than 0 and less than 11!")
+            exit()
 
+        self.ticks = self.adjust_list(self.ticks, self.ttask)
 
-    def adjust_list(self, ticks, ttask):
-        for _ in range(ttask - 1): 
+    @staticmethod
+    def adjust_list(ticks: list, ttask: int) -> list:
+        """Adjust the list that stores user tasks.
+
+        :param ticks: Receive the list of ticks
+        :param ttask: Receive the size of the default tasks.
+        :return: Returns a list of tasks adjusted according to the input.
+        """
+        for _ in range(ttask - 1):
             ticks.append(0)
         return ticks
 
-    
-    def process(self):
-        self.ticks =  self.adjust_list(self.ticks, self.ttask)
+    def remove_machines(self, counter_tick: int) -> None:
+        """Remove the tasks and removes machines no longer used.
+
+        :param counter_tick: Counter of ticks from the beginning of
+        the execution.
+        """
+        exclude_machine = []
+        for each in self.open_machines:
+            each.close_processed_task(counter_tick)
+            if each.open_slots == self.umax:
+                exclude_machine.append(each)
+
+        for each in exclude_machine:
+            self.open_machines.pop(self.open_machines.index(each))
+            if self.last_machine_id > 0:
+                self.last_machine_id -= 1
+
+    def calculate_cost_and_call_output(self) -> None:
+        """Calculate the cost of machines and calls to write to the output."""
+        machines_open_tasks = []
+        for each in self.open_machines:
+            if each.open_tasks != 0:
+                machines_open_tasks.append(
+                    each.open_tasks)
+
+        self.total_cost += len(machines_open_tasks)
+        if not machines_open_tasks:
+            machines_open_tasks.append(0)
+        self.set_output(machines_open_tasks)
+
+    def process(self) -> None:
+        """Allocating the tacks on machines the smallest number possible."""
         for counter_tick, amount_task in enumerate(self.ticks, 1):
-            machines_open_tasks = []
+
+            self.remove_machines(counter_tick)
+
             if not self.open_machines and amount_task > 0:
                 self.open_machines.append(Machine(self.umax))
-            
-            for each_machine in self.open_machines:
 
-                each_machine.close_processed_task(counter_tick)
+            while amount_task:
+                each_machine = self.open_machines[self.last_machine_id]
 
-                while amount_task:
-                    amount_task = each_machine.up_tasks(
-                        counter_tick, 
-                        amount_task, 
-                        self.ttask)
-                    if all([
-                        amount_task > 0,
-                        each_machine.open_tasks == self.umax,
-                        self.open_machines.index(
-                            each_machine) == len(self.open_machines) - 1]):
+                amount_task = each_machine.up_tasks(
+                    counter_tick,
+                    amount_task,
+                    self.ttask)
+                if all([amount_task > 0,
+                        each_machine.open_tasks == self.umax]):
 
-                        self.open_machines.append(Machine(self.umax))
-                        break
-                    else:
-                        break
-                # if each_machine.open_slots == self.umax:
-                #     self.open_machines.pop(self.open_machines.index(each_machine))
-                if each_machine.open_tasks != 0:
-                    machines_open_tasks.append(
-                        each_machine.open_tasks)
+                    self.open_machines.append(Machine(self.umax))
+                    self.last_machine_id += 1
 
-            self.total_cost += len(machines_open_tasks)
-            self.set_output(machines_open_tasks)
-        print(self.total_cost)
+            self.calculate_cost_and_call_output()
 
-    def run(self):
-        if os.path.exists("output.txt"):
-            os.remove("output.txt")
+        self.set_output(self.total_cost)
+
+    def run(self) -> None:
+        """Responsible for starting execution."""
+        if os.path.exists(self.output_file_name):
+            os.remove(self.output_file_name)
         self.begin_process()
         self.process()
 
+
 if __name__ == "__main__":
+    """
+    Program start.
+    """
     load_balancer = LoadBalancer()
     load_balancer.run()
---
+++ test/test_load_balancer.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+from load_balancer import  Machine, LoadBalancer
+
+class TestLoad(TestCase):
+    def setUp(self):
+        self.machine = Machine(2)
+        self.load_balancer = LoadBalancer()
+
+    def test_up_tasks(self):
+        machine = Machine(2)
+        self.assertEqual(self.machine.up_tasks(4, 1, 2), 0)
+
+    def testando_a_maquina_executa_as_tasks_e_retorna_o_valor_das_que_n_conseguiu_executar(self):
+        self.assertEqual(self.machine.up_tasks(4, 6, 2), 4)
+        
+
+    def testando_a_maquina_fecha_as_tasks_e_retorna_none(self):
+        self.assertEqual(self.machine.close_processed_task(6), None)
+        self.assertEqual(len(self.machine.tasks_death_tick), 0)
+
+    def testando_a_maquina_o_valor_que_a_maquina_criou_na_lista(self):
+        self.assertEqual(self.machine.up_tasks(4, 6, 2), 4)
+        self.assertEqual(self.machine.tasks_death_tick[0], 6)
+
+    def testando_se_adjust_list_retorna_a_lista_ajustada_coretamente(self):
+        lista = [1,2,3,4]
+        self.assertEqual(len(self.load_balancer.adjust_list(lista, 4)), 7)
